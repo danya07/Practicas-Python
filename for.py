596ef6f7d6cb7013ccc1e23f593f420f2504cc34
Danya Carrera <danyapin07@gmail.com>
2021-09-04 16:29:01 -0700

    Ciclo for

---
+++ for.py
@@ -0,0 +1,14 @@
+#contador = 1
+#print(contador)
+#while contador < 2999:
+    #contador += 1
+    #print(contador)
+
+# a = list(range(1000))
+# print(a)
+
+# for contador in range(0, 3001):
+#     print(contador)
+
+for i in range(21):
+    print(22 * i)
